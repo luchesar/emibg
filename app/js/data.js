8d0e750f1212d454cd8626913bdce048a282c74b
Luchesar Cekov <luchesar@gmail.com>
2015-05-26 22:00:45 +0100

    Add npm docker wrapper and some data for presentation

---
+++ app/js/data.js
@@ -0,0 +1,43 @@
+var articles = [
+   { 
+     id: "1",
+     title: {
+       bg: "Статия 1",
+       en: "Article one"
+     },
+     category: ["analysis", "emis"],
+     tumb: "thumbs/thumb1.jpg",
+     author: {
+       bg: "Пейчо Пейковски",
+       en: "Peicho Peikowski"
+     },
+     date: "23/12/2014T21:45:53.234",
+     html: {
+       bg: "Това е статия едно",
+       en: "This is article one"
+     }
+   },
+   { 
+     id: "2",
+     title: {
+       bg: "Article 2",
+       en: ""
+     },
+     category: ["news"],
+     tumb: "thumbs/thumb2.jpg",
+     author: {
+       bg: "Трайчо Косто",
+       en: ""//no english translation
+     },
+     date: "23/12/2014T21:45:53.234",
+     html: {
+       bg: "Това е статия две",
+       en: "This is article two"
+     },
+     tags: ["tag1", "tag2"]
+   }
+]
+
+var categories = [
+    "news", "analysis", "emis", "resumes", "events", "energopedia", "partners"
+]
