4cbaa2eae2493a544c35fa42c9ede63b0d00b287
Luchesar Cekov <luchesar@gmail.com>
2015-05-27 22:06:35 +0100

    Menu bar WIP

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,39 +1,39 @@
-'use strict';
+    'use strict';
 
-/**
- * @ngInject
- */
-function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
+    /**
+     * @ngInject
+     */
+    function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
 
-  $locationProvider.html5Mode(true);
+      $locationProvider.html5Mode(true);
 
-  $stateProvider
-  .state('app', {
-    url: '/{lang:(?:bg|en)}',
-    abstract: true,
-    template: '<ui-view/>'
-  })
-  .state('app.home', {
-    url: '',
-    templateUrl: 'home.html'
-  })
-  .state('app.home.news', {
-    url: '/news',
-    template: '<div>news</div>'
-  })
-  .state('app.home.analysis', {
-    url: '/analysis',
-    template: '<div>analysis</div>'
-  })
-  .state('app.home.events', {
+      $stateProvider
+      .state('app', {
+        url: '/{lang:(?:bg|en)}',
+        abstract: true,
+        template: '<ui-view/>'
+      })
+      .state('app.menu', {
+        url: '',
+        templateUrl: 'menu.html'
+      })
+      .state('app.menu.news', {
+        url: '/news',
+        template: '<div>news</div>'
+      })
+      .state('app.menu.analysis', {
+        url: '/analysis',
+        template: '<div>analysis</div>'
+      })
+  .state('app.menu.events', {
     url: '/events',
     template: '<div>events</div>'
   })
-  .state('app.home.energopedia', {
+  .state('app.menu.energopedia', {
     url: '/energopedia',
     template: '<div>energopedia</div>'
   })
-  .state('app.home.partners', {
+  .state('app.menu.partners', {
     url: '/partners',
     template: '<div>partners</div>'
   });
