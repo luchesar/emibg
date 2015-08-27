fb739cd80614b36e7cf585d0d31320bdcb36c4e9
Luchesar Cekov <luchesar@gmail.com>
2015-08-27 16:06:15 +0100

    Some more text in the slider along with better footer

--- app/js/controllers/slider.js
+++ app/js/controllers/slider.js
@@ -10,15 +10,18 @@ function SliderCtrl($scope) {
   var slides = $scope.slides = [
     {
       image: 'images/slider/slide1.jpg',
-      text: ''
+      title: 'Record boost in new solar power continues massive industry growth',
+      text: 'UK leads European solar energy expansion to help renewables overtake output of nuclear power as industry leaders hail ‘tipping point’ for the technology'
     },
     {
       image: 'images/slider/slide2.jpg',
-      text: ''
+      title: 'South African team may have solved solar puzzle even Google couldn\'t crack',
+      text: 'Pioneering technology to deliver the cheapest, small-scale concentrated solar power plants in the world could revolutionise the renewable energy market'
     },
     {
       image: 'images/slider/slide3.jpg',
-      text: ''
+      title: 'The future is kite powered',
+      text: 'Wind turbines are controversial. They are accused of being blots on the landscape, expensive to run and need good winds to work. But the critics could be silenced with a totally new way of generating wind power – using kites. This is a serious proposition. Power-generating kites are far more sophisticated'
     }
   ];
   
