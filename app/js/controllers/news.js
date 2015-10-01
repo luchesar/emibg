6f30c935207840e9148fe5c3f452e436ca9c6dae
Luchesar Cekov <luchesar@gmail.com>
2015-10-01 18:04:16 +0100

    Abstracting out the paging functionality from the news controller into a service

--- app/js/controllers/news.js
+++ app/js/controllers/news.js
@@ -5,22 +5,9 @@ var controllersModule = require('./_index');
 /**
 * @ngInject
 */
-function NewsCtrl($scope, $stateParams, $state, ArticleService) {
-  var pageNumber = function() {
-    var p = $stateParams.page;
-    if (p < 1) {
-      return 1;
-    } else if (p > $scope.pageCount) {
-      return $scope.pageCount;
-    } else {
-      return p;
-    }
-  }
-
-  $scope.itemsPerPage = 6;
+function NewsCtrl($scope, $stateParams, $state, ArticleService, PagingService) {
   $scope.itemsCount = ArticleService.filterSize(['news']);
-  $scope.pageCount = Math.ceil($scope.itemsCount / $scope.itemsPerPage);
-  $scope.page = pageNumber();
+  PagingService.init($scope, $stateParams, $state);
 
   $scope.news = ArticleService.filterPaged(
     ['news'],
@@ -28,30 +15,6 @@ function NewsCtrl($scope, $stateParams, $state, ArticleService) {
     $scope.itemsPerPage,
     2
   );
-
-  $scope.$watch('page', function() {
-    $state.go('.', {page: $scope.page});
-  });
-
-  $scope.hasNext = function() {
-    return $scope.page * $scope.itemsPerPage < $scope.itemsCount;
-  }
-
-  $scope.hasPrev = function() {
-    return $scope.page > 1;
-  }
-
-  $scope.nextPage = function() {
-    if ($scope.hasNext()) {
-      $scope.page = $scope.page + 1;
-    }
-  };
-
-  $scope.previousPage = function() {
-    if ($scope.hasPrev()) {
-      $scope.page = $scope.page - 1;
-    }
-  };
 }
 
 controllersModule.controller('NewsCtrl', NewsCtrl);
---
+++ app/js/services/paging.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var servicesModule = require('./_index.js');
+
+function PagingService() {
+  var service = {};
+
+  var pageNumber = function($scope, $stateParams) {
+    var p = $stateParams.page;
+    if (p < 1) {
+      return 1;
+    } else if (p > $scope.pageCount) {
+      return $scope.pageCount;
+    } else {
+      return p;
+    }
+  }
+
+  service.init = function($scope, $stateParams, $state) {
+    $scope.itemsPerPage = 6;
+    $scope.pageCount = Math.ceil($scope.itemsCount / $scope.itemsPerPage);
+    $scope.page = pageNumber($scope, $stateParams);
+
+    $scope.$watch('page', function() {
+      $state.go('.', {page: $scope.page});
+    });
+
+    $scope.hasNext = function() {
+      return $scope.page * $scope.itemsPerPage < $scope.itemsCount;
+    }
+
+    $scope.hasPrev = function() {
+      return $scope.page > 1;
+    }
+
+    $scope.nextPage = function() {
+      if ($scope.hasNext()) {
+        $scope.page = $scope.page + 1;
+      }
+    };
+
+    $scope.previousPage = function() {
+      if ($scope.hasPrev()) {
+        $scope.page = $scope.page - 1;
+      }
+    };
+  }
+
+  return service;
+}
+
+servicesModule.service('PagingService', PagingService);
