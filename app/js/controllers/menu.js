9a6f154fc248ad728a4bcfde52fa2ed8236aa000
Luchesar Cekov <luchesar@gmail.com>
2015-10-31 21:49:34 +0000

    Showing search

--- app/js/controllers/menu.js
+++ app/js/controllers/menu.js
@@ -6,15 +6,25 @@ var _ = require('lazy.js');
 /**
 * @ngInject
 */
-function MenuCtrl($scope, ArticleService, EventService) {
+function MenuCtrl($scope, $timeout) {
+  $scope.search = {text: ""};
+
   $scope.searchVisible = false;
+
   $scope.showSearch = function() {
       $scope.searchVisible = true;
-      $("#searchField").focus();
+      $timeout(function(){
+          $("#searchField").focus();
+      });
   };
+
   $scope.hideSearch = function() {
       $scope.searchVisible = false;
   };
+
+  $scope.$watch('search.text', function (val) {
+     console.log(val);
+  });
 }
 
 controllersModule.controller('MenuCtrl', MenuCtrl);
