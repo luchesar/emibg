e0a3b7a2393526adc2feccb3fe26033939a255fc
Luchesar Cekov <luchesar@gmail.com>
2015-12-31 21:46:42 +0000

    Viewing a single article works

--- app/js/controllers/full-event.js
+++ app/js/controllers/full-event.js
@@ -6,8 +6,11 @@ var controllersModule = require('./_index');
 * @ngInject
 */
 function FullEventCtrl($scope, $sce, $stateParams, EventService, $filter) {
-  $scope.event = EventService.event($stateParams.id);
-  $scope.eventHtml = $sce.trustAsHtml($filter('lang')($scope.event.html));
+  EventService.event($stateParams.id)
+  .then(function(event) {
+    $scope.event = event;
+    $scope.eventHtml = $sce.trustAsHtml($filter('lang')($scope.event.html));
+  });
 }
 
 controllersModule.controller('FullEventCtrl', FullEventCtrl);
