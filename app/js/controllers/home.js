c73f50245a85f98b2387bc0ae3e869eab18d3728
Luchesar Cekov <luchesar@gmail.com>
2016-01-02 14:11:59 +0000

    Home slider partially from the db

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -2,37 +2,40 @@
 
 var controllersModule = require('./_index');
 var _ = require('lazy.js');
-var data = require('../data.js');
 
 /**
 * @ngInject
 */
-function HomeCtrl($scope, ArticleService, EventService) {
-  var news = ArticleService.filter(['news'], 6);
-  var analysis = ArticleService.filter(['analysis'], 5);
-  var events = EventService.filter().take(5);
+function HomeCtrl($scope, HomeItemsSlider, ArticleService, EventService) {
+  ArticleService.filter(['news'], 6).then(function(news) {
+    $scope.mainNews = _(news).take(1).toArray();
+    $scope.news = _(news).drop(1).toArray();
+  });// TODO show the error
 
-  $scope.slideItems = data.homeItemsSlider.map(function(slide){
-  	var item = null;
-  	if (slide.type === "article") {
-  		item = ArticleService.article(slide.id);
-  		item.sref = ".article({ id:" + slide.id + " })";
-  	} else if (slide.type === "event") {
-  		item = EventService.event(slide.id);
-  		item.sref = ".event({ id:" + slide.id + " })";
-  	}
-  	item.type = slide.type;
-  	return item;
+  ArticleService.filter(['emis'], 5).then(function(analysis){
+    $scope.mainAnalysis = _(analysis).take(1).toArray();
+    $scope.analysis = _(analysis).drop(1).toArray();
+  });// TODO show the error
+
+  EventService.filter(5).then(function(events) {
+    $scope.mainEvent = _(events).take(1).toArray();
+    $scope.events = _(events).drop(1).toArray();
+  });// TODO show the error
+
+  HomeItemsSlider.find().$promise.then(function(slides) {
+    $scope.slideItems = slides.map(function(slide){
+          var item = null;
+          if (slide.type === "article") {
+                  item = ArticleService.article(slide.id);
+                  item.sref = ".article({ id:" + slide.id + " })";
+          } else if (slide.type === "event") {
+                  item = EventService.event(slide.id);
+                  item.sref = ".event({ id:" + slide.id + " })";
+          }
+          item.type = slide.type;
+          return item;
+    });
   });
-
-  $scope.mainNews = news.take(1).toArray();
-  $scope.news = news.drop(1).toArray();
-
-  $scope.mainAnalysis = analysis.take(1).toArray();
-  $scope.analysis = analysis.drop(1).toArray();
-
-  $scope.mainEvent = events.take(1).toArray();
-  $scope.events = events.drop(1).toArray();
 }
 
 controllersModule.controller('HomeCtrl', HomeCtrl);
