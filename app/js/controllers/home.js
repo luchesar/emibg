bb1fef8c901015de91ce2d15747344bc56ea5a98
Luchesar Cekov <luchesar@gmail.com>
2016-01-04 18:56:29 +0000

    Use the unified service for the home-page

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -6,42 +6,19 @@ var _ = require('lazy.js');
 /**
 * @ngInject
 */
-function HomeCtrl($scope, $q, HomeItemsSlider, ArticleService, EventService) {
-  ArticleService.filter(['news'], 6).then(function(news) {
-    $scope.mainNews = _(news).take(1).toArray();
-    $scope.news = _(news).drop(1).toArray();
-  });// TODO show the error
+function HomeCtrl($scope, $http, $stateParams) {
+  $http.get("/api/home-pages/" + $stateParams.lang || "bg")
+  .then(function(response) {
+    $scope.mainNews = _(response.data.news).take(1).toArray();
+    $scope.news = _(response.data.news).drop(1).toArray();
 
-  ArticleService.filter(['emis'], 5).then(function(analysis){
-    $scope.mainAnalysis = _(analysis).take(1).toArray();
-    $scope.analysis = _(analysis).drop(1).toArray();
-  });// TODO show the error
+    $scope.mainAnalysis = _(response.data.analysis).take(1).toArray();
+    $scope.analysis = _(response.data.analysis).drop(1).toArray();
 
-  EventService.filter(5).then(function(events) {
-    $scope.mainEvent = _(events).take(1).toArray();
-    $scope.events = _(events).drop(1).toArray();
-  });// TODO show the error
+    $scope.mainEvent = _(response.data.events).take(1).toArray();
+    $scope.events = _(response.data.events).drop(1).toArray();
 
-  HomeItemsSlider.find().$promise.then(function(slides) {
-    var promises = slides.map(function(slide) {
-      if (slide.type === "article") {
-        return ArticleService.article(slide.itemId)
-          .then(function(article) {
-            article.sref = ".article({ id:'" + slide.itemId + "' })";
-            article.type = slide.type;
-            return article;
-          });
-      } else if (slide.type === "event") {
-        return EventService.event(slide.itemId)
-          .then(function(event) {
-            event.sref = ".event({ id:'" + slide.itemId + "' })";
-            event.type = slide.type;
-            return event;
-          });
-      }
-    });
-    $q.all(promises)
-    .then(slideItems => $scope.slideItems = slideItems);
+    $scope.slideItems = response.data.slides;
   });
 }
 
