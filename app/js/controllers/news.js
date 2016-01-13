675e14acf573c3ca87612530027eb705b30b3761
Luchesar Cekov <luchesar@gmail.com>
2016-01-13 21:19:29 +0000

    Using the new paging in the news controller

--- app/js/controllers/news.js
+++ app/js/controllers/news.js
@@ -1,23 +1,33 @@
 'use strict'
 
 var controllersModule = require('./_index');
+var _ = require('lazy.js');
 
 /**
 * @ngInject
 */
-function NewsCtrl($scope, $stateParams, $state, ArticleService, PagingService) {
+function NewsCtrl($scope, $stateParams, $http, $state, PagingService) {
   $scope.pageCount = "Loading";
-  ArticleService.filterSize(['news'])
-  .then(function(count) {
-    $scope.itemsCount = count.count;
-    PagingService.init($scope, $stateParams, $state);
-    ArticleService.filterPaged(
-      ['news'],
-      $scope.page,
-      $scope.itemsPerPage,
-      2
-    ).then(items => $scope.news = items);
- });
+  PagingService.init($scope, $stateParams, $state, function(){
+    $state.go('.', {page: $scope.page});
+  });
+
+
+  $http.get(
+    "/api/articles/paged/" +
+    ($stateParams.lang ||  "bg") +
+    "?categories=emis&p=" + PagingService.pageNumber($stateParams) +
+    "&size=" + PagingService.itemsPerPage
+  )
+  .then(function(response) {
+    $scope.itemsCount = response.data.size;
+    $scope.pageCount = PagingService.pageCount($scope.itemsCount);
+    $scope.news = _(response.data.items)
+        .chunk(2).toArray();
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 }
 
 controllersModule.controller('NewsCtrl', NewsCtrl);
