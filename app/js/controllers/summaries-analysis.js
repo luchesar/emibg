0a7e40ca801c43518437d38a6bf50549acf7dec2
Luchesar Cekov <luchesar@gmail.com>
2016-01-13 21:30:02 +0000

    add paging for emsummaries nalisys

--- app/js/controllers/summaries-analysis.js
+++ app/js/controllers/summaries-analysis.js
@@ -6,17 +6,27 @@ var _ = require('lazy.js');
 /**
 * @ngInject
 */
-function SummariesAnalysisCtrl($scope, $stateParams, $state, ArticleService, PagingService) {
-  ArticleService.filterSize(['summaries'])
-  .then(function(count) {
-    $scope.itemsCount = count.count;
-    PagingService.init($scope, $stateParams, $state);
-    ArticleService.filterPaged(
-      ['summaries'],
-      $scope.page,
-      $scope.itemsPerPage,
-      2
-    ).then(items => $scope.summariesAnalysis = items);
+function SummariesAnalysisCtrl($scope, $stateParams, $http, $state, PagingService) {
+  $scope.pageCount = "Loading";
+  PagingService.init($scope, $stateParams, $state, function(){
+    $state.go('.', {page: $scope.page});
+  });
+
+
+  $http.get(
+    "/api/articles/paged/" +
+    ($stateParams.lang ||  "bg") +
+    "?categories=summaries&p=" + PagingService.pageNumber($stateParams) +
+    "&size=" + PagingService.itemsPerPage
+  )
+  .then(function(response) {
+    $scope.itemsCount = response.data.size;
+    $scope.pageCount = PagingService.pageCount($scope.itemsCount);
+    $scope.summariesAnalysis = _(response.data.items)
+        .chunk(2).toArray();
+  })
+  .catch(function(err) {
+    console.log(err);
   });
 }
 
