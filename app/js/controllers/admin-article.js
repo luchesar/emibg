bdef0fa2318aa879185d5d0e60b240e6d954b040
Luchesar Cekov <luchesar@gmail.com>
2016-01-19 20:02:16 +0000

    save articles

--- app/js/controllers/admin-article.js
+++ app/js/controllers/admin-article.js
@@ -5,25 +5,63 @@ var controllersModule = require('./_index');
 /**
 * @ngInject
 */
-function AdminArticleCtrl($scope, $sce, $stateParams, ArticleService, $filter) {
-  $scope.tinymceOptions = {
-    onChange: function(e) {
-      // put logic here for keypress and cut/paste changes
-    },
-    inline: true,
+function AdminArticleCtrl($scope, $stateParams, ArticleService, $filter, $rootScope, $state, Articles, $http) {
+  $scope.previousState = $rootScope.previousState;
+  $scope.previousStateParams = $rootScope.previousStateParams;
+
+  $scope.cancel = function() {
+    if ($rootScope.previousState) {
+      $state.go($scope.previousState, $scope.previousStateParams);
+    } else {
+      $state.go("app.admin.articles");
+    }
+  };
+
+  $scope.save = function() {
+    $scope.article.html.bg = $scope.editedHtml;
+    $scope.article.title.bg = $scope.editedTitle;
+    $http.put("/api/articles/" + $scope.article.id, $scope.article)
+    .then(function(response) {
+      console.log("article saved");
+    })
+    .catch(function(err){
+       $scope.err = err;
+       console.log(err);
+     })
+  }
+
+  var editorOptions = function(options, onChange) {
+    options.inline = true;
+    options.skin = 'lightgray';
+    options.theme = 'modern';
+    options.browser_spellcheck = true;
+    options.init_instance_callback = function(editor) {
+      var textContentTrigger = function() {
+        onChange($scope, editor);
+      };
+
+      editor.on('KeyUp', textContentTrigger);
+      editor.on('ExecCommand', textContentTrigger);
+      editor.on('SetContent', function(e) {
+        if (!e.initial)
+          textContentTrigger();
+      });
+    };
+    return options;
+  }
+
+  $scope.tinymceOptions = editorOptions({
     plugins : 'advlist autolink link image lists charmap print preview',
-    skin: 'lightgray',
-    theme : 'modern',
-    browser_spellcheck: true
-  };
-  $scope.tinymceTitleOptions = {
-    inline: true,
+  }, function($scope, editor) {
+    $scope.editedHtml = jQuery(editor.getElement()).html();
+  });
+
+  $scope.tinymceTitleOptions = editorOptions({
     toolbar: 'undo redo',
-    menubar: false,
-    skin: 'lightgray',
-    theme : 'modern',
-    browser_spellcheck: true
-  };
+    menubar: false
+  }, function($scope, editor) {
+    $scope.editedTitle = jQuery(editor.getElement()).text();
+  });
 
   ArticleService.article($stateParams.id)
   .then(function(article) {
