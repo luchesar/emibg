ecac39828d91737709d39f1d2c286913c32afd3d
Luchesar Cekov <luchesar@gmail.com>
2016-01-21 14:40:50 +0000

    Events editor

---
+++ app/js/controllers/admin-event.js
@@ -0,0 +1,110 @@
+'use strict'
+
+var controllersModule = require('./_index');
+var _ = require('lazy.js');
+
+/**
+* @ngInject
+*/
+function AdminEventCtrl($scope, $stateParams, EventService, $filter, $rootScope, $state, Events, $http, $sce) {
+  $scope.alerts = [];
+  $scope.previousState = $rootScope.previousState;
+  $scope.previousStateParams = $rootScope.previousStateParams;
+
+  $scope.cancel = function() {
+    if ($rootScope.previousState) {
+      $state.go($scope.previousState, $scope.previousStateParams);
+    } else {
+      $state.go("app.admin.articles");
+    }
+  };
+
+  var nullify = function(field) {
+    if(field.bg != null && field.bg != undefined && field.bg.trim() === "")
+      field.bg = null;
+    if(field.en != null && field.en != undefined && field.en.trim() === "")
+      field.en = null;
+  }
+
+  $scope.save = function() {
+    $scope.alerts = [];
+
+    // Remove the empty props to be able to filter with exists in ES
+    nullify($scope.event.title);
+
+    $http.put("/api/events/" + $scope.event.id, $scope.event)
+    .then(function(response) {
+      $scope.alerts.push({type: 'success', msg: $sce.trustAsHtml("Събитието е записана успещно")});
+    })
+    .catch(function(err) {
+      $scope.alerts.push({type: 'danger', msg: "Не е възможно да се запише събитието в момента. Моля опитайте след малко."});
+    });
+  }
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
+
+  var editorOptions = function(options, onChange) {
+    options.inline = true;
+    options.language = $stateParams.lang || 'bg',
+    options.skin = 'lightgray';
+    options.theme = 'modern';
+    options.browser_spellcheck = true;
+    options.init_instance_callback = function(editor) {
+      var textContentTrigger = function() {
+        onChange(editor);
+      };
+
+      editor.on('KeyUp', textContentTrigger);
+      editor.on('ExecCommand', textContentTrigger);
+      editor.on('SetContent', function(e) {
+        if (!e.initial)
+          textContentTrigger();
+      });
+    };
+    return options;
+  }
+
+  var htmlOptions = function(updateProperty) {
+    return editorOptions({
+      plugins : "advlist autolink link image imagetools lists charmap print preview autolink lists spellchecker pagebreak layer table save insertdatetime preview media searchreplace print contextmenu paste directionality fullscreen noneditable visualchars nonbreaking template google_tools placeholder",
+      }, function(editor) {
+        updateProperty(jQuery(editor.getElement()).html());
+      });
+  }
+
+  var titleOptions = function(updateProperty) {
+    return editorOptions({
+        toolbar: 'undo redo',
+        menubar: false
+      }, function(editor) {
+        updateProperty(jQuery(editor.getElement()).text());
+      });
+  }
+
+  $scope.htmlBgOptions = htmlOptions(function(html) {
+      $scope.event.html.bg = html;
+      $scope.bgHtml = $sce.trustAsHtml(html);
+  });
+  $scope.htmlEnOptions = htmlOptions(function(html) {
+      $scope.event.html.en = html;
+      $scope.enHtml = $sce.trustAsHtml(html);
+  });
+  $scope.titleBgOptions = titleOptions(title => $scope.event.title.bg = title);
+  $scope.titleEnOptions = titleOptions(title => $scope.event.title.en = title);
+
+  EventService.event($stateParams.id)
+  .then(function(event) {
+    $scope.event = event;
+    
+    $scope.title = JSON.parse(JSON.stringify(event.title));
+    $scope.html = JSON.parse(JSON.stringify(event.html));
+    $scope.bgHtml = $sce.trustAsHtml(event.html.bg);
+    $scope.enHtml = $sce.trustAsHtml(event.html.bg);
+  })
+  .catch(err => $scope.alerts.push({type: 'danger', msg: err + ""}));
+}
+
+controllersModule.controller('AdminEventCtrl', AdminEventCtrl);
