098dc6817e4a4dcefe832ed245b0b1ca8bee7e02
Luchesar Cekov <luchesar@gmail.com>
2016-02-04 21:52:47 +0000

    scroll to top for articles and events

--- app/js/controllers/full-event.js
+++ app/js/controllers/full-event.js
@@ -6,7 +6,7 @@ var controllersModule = require('./_index');
 * @ngInject
 */
 function FullEventCtrl($scope, $sce, $stateParams, EventService, $filter, $document) {
-  $document.scrollTopAnimated(0, 150);
+  $document.scrollTopAnimated(0, 190);
   EventService.event($stateParams.id)
   .then(function(event) {
     $scope.event = event;
