13c22a92ae2b8ffe4a3f30abdebfacd041e6c898
Luchesar Cekov <luchesar@gmail.com>
2016-02-26 16:29:05 +0000

    Add page titles

--- app/js/routes.js
+++ app/js/routes.js
@@ -30,7 +30,8 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
       'right-column@': {template: ''},
       'footer@': {template: ''},
       'content@': { templateUrl: 'login.html' }
-    }
+    },
+    data: { title: "LOGIN"}
   })
   .state('app.search', {
     url: '',
@@ -41,15 +42,18 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
   })
   .state('app.search.result', {
     url: '/search?{q:string}&{page:int}',
-    templateUrl: 'search.html'
+    templateUrl: 'search.html',
+    data: { title: "SEARCH"}
   })
   .state('app.search.result.event', {
     url: '/event/{id:string}',
-    views: {'content@': { templateUrl: 'fragments/full-event.html' }}
+    views: {'content@': { templateUrl: 'fragments/full-event.html' }},
+    data: { title: "EVENTS"}
   })
   .state('app.search.result.article', {
     url: '/article/{id:string}',
-    views: { 'content@': { templateUrl: 'fragments/full-article.html' }}
+    views: { 'content@': { templateUrl: 'fragments/full-article.html' }},
+    data: { title: "ARTICLES"}
   })
   .state('app.menu', {
     url: '',
@@ -60,19 +64,23 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
   })
   .state('app.menu.home', {
     url: '/home',
-    templateUrl: 'fragments/home.html'
+    templateUrl: 'fragments/home.html',
+    data: { title: "HOME"}
   })
   .state('app.menu.home.event', {
     url: '/event/{id:string}',
-    views: {'content@': { templateUrl: 'fragments/full-event.html' }}
+    views: {'content@': { templateUrl: 'fragments/full-event.html' }},
+    data: { title: "EVENTS"}
   })
   .state('app.menu.home.article', {
     url: '/article/{id:string}',
-    views: {'content@': { templateUrl: 'fragments/full-article.html' }}
+    views: {'content@': { templateUrl: 'fragments/full-article.html' }},
+    data: { title: "ARTICLES"}
   })
   .state('app.menu.news', {
     url: '/news/{page:int}',
-    templateUrl: 'fragments/news.html'
+    templateUrl: 'fragments/news.html',
+    data: { title: "NEWS"}
   })
   .state('app.menu.news.article', {
     url: '/article/{id:string}',
@@ -85,7 +93,8 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
   })
   .state('app.menu.analysis.emis', {
     url: '/emis/{page:int}',
-    templateUrl: 'fragments/emis-analysis.html'
+    templateUrl: 'fragments/emis-analysis.html',
+    data: { title: "EMIS_ANALYSIS"}
   })
   .state('app.menu.analysis.emis.article', {
     url: '/article/{id:string}',
@@ -93,7 +102,8 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
   })
   .state('app.menu.analysis.summaries', {
 url: '/summaries/{page:int}',
-    templateUrl: 'fragments/summaries-analysis.html'
+    templateUrl: 'fragments/summaries-analysis.html',
+    data: { title: "SUMMARIES"}
   })
   .state('app.menu.analysis.summaries.article', {
     url: '/article/{id:string}',
@@ -101,7 +111,8 @@ url: '/summaries/{page:int}',
   })
   .state('app.menu.events', {
     url: '/events/{page:int}',
-    templateUrl: 'fragments/events.html'
+    templateUrl: 'fragments/events.html',
+    data: { title: "EVENTS"}
   })
   .state('app.menu.events.event', {
     url: '/event/{id:string}',
@@ -109,11 +120,13 @@ url: '/summaries/{page:int}',
   })
   .state('app.menu.energopedia', {
     url: '/energopedia',
-    templateUrl: 'fragments/energopedia.html'
+    templateUrl: 'fragments/energopedia.html',
+    data: { title: "ENERGOPEDIA"}
   })
   .state('app.menu.partners', {
     url: '/partners',
-    templateUrl: 'fragments/partners.html'
+    templateUrl: 'fragments/partners.html',
+    data: { title: "PARTNERS"}
   })
   .state('app.menu.about', {
     url: '/about',
@@ -122,23 +135,28 @@ url: '/summaries/{page:int}',
   })
   .state('app.menu.about.mission', {
     url: '/mission',
-    templateUrl: 'fragments/about_mission.html'
+    templateUrl: 'fragments/about_mission.html',
+    data: { title: "MISSION"}
   })
   .state('app.menu.about.team', {
     url: '/team',
-    templateUrl: 'fragments/about_team.html'
+    templateUrl: 'fragments/about_team.html',
+    data: { title: "TEAM"}
   })
   .state('app.menu.about.contact', {
     url: '/contact',
-    templateUrl: 'fragments/about_contact.html'
+    templateUrl: 'fragments/about_contact.html',
+    data: { title: "CONTACT"}
   })
   .state('app.menu.article', {
     url: '/article/{id:string}',
-    templateUrl: 'fragments/full-article.html'
+    templateUrl: 'fragments/full-article.html',
+    data: { title: "ARTICLES"}
   })
   .state('app.menu.chart', {
     url: '/chart/{id:string}',
-    templateUrl: 'fragments/single-chart.html'
+    templateUrl: 'fragments/single-chart.html',
+    data: { title: "EVENTS"}
   })
 
   // Admin states
@@ -154,43 +172,53 @@ url: '/summaries/{page:int}',
   })
   .state('app.admin.articles', {
     url: '/articles/{page:int}?published&showCategories',
-    templateUrl: 'admin/articles.html'
+    templateUrl: 'admin/articles.html',
+    data: { title: "ARTICLES"}
   })
   .state('app.admin.events', {
     url: '/events/{page:int}?published',
-    templateUrl: 'admin/events.html'
+    templateUrl: 'admin/events.html',
+    data: { title: "EVENTS"}
   })
   .state('app.admin.charts', {
     url: '/charts/{page:int}',
-    templateUrl: 'admin/charts.html'
+    templateUrl: 'admin/charts.html',
+    data: { title: "CHARTS"}
   })
   .state('app.admin.article', {
     url: '/article/{id:string}',
-    templateUrl: 'admin/article.html'
+    templateUrl: 'admin/article.html',
+    data: { title: "ARTICLES"}
   })
   .state('app.admin.newarticle', {
     url: '/newarticle',
-    templateUrl: 'admin/article.html'
+    templateUrl: 'admin/article.html',
+    data: { title: "ARTICLES"}
   })
   .state('app.admin.event', {
     url: '/event/{id:string}',
-    templateUrl: 'admin/event.html'
+    templateUrl: 'admin/event.html',
+    data: { title: "EVENTS"}
   })
   .state('app.admin.newevent', {
     url: '/newevent',
-    templateUrl: 'admin/event.html'
+    templateUrl: 'admin/event.html',
+    data: { title: "EVENTS"}
   })
   .state('app.admin.chart', {
     url: '/chart/{id:string}',
-    templateUrl: 'admin/chart.html'
+    templateUrl: 'admin/chart.html',
+    data: { title: "CHARTS"}
   })
   .state('app.admin.newchart', {
     url: '/newchart/{type:string}',
-    templateUrl: 'admin/chart.html'
+    templateUrl: 'admin/chart.html',
+    data: { title: "CHARTS"}
   })
   .state('app.admin.homepage', {
     url: '/homepage',
-    templateUrl: 'admin/homepage.html'
+    templateUrl: 'admin/homepage.html',
+    data: { title: "HOME"}
   })
   ;
 }
