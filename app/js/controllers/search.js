6427db4cf51840c8de3ebc2b6da76d4978f01845
Luchesar Cekov <luchesar@gmail.com>
2016-03-02 16:54:00 +0000

    Error handling for the search screen

--- app/js/controllers/search.js
+++ app/js/controllers/search.js
@@ -3,17 +3,18 @@
 var controllersModule = require('./_index');
 var _ = require('lazy.js');
 
-function SearchCtrl($scope, $stateParams, $http, $state, PagingService) {
+function SearchCtrl($scope, $stateParams, $http, $state, PagingService, ErrorHandling) {
   PagingService.init($scope, $stateParams, $state, function(){
     $state.go('.', {q: $stateParams.q, page: $scope.page});
   });
 
   var lang = $stateParams.lang || "bg";
 
-  $http.get("/api/searches/" + lang +
+  $scope.alerts = [];
+  ErrorHandling.handle($http.get("/api/searches/" + lang +
            "?q=" + ($stateParams.q || "*") +
            "&p=" + PagingService.pageNumber($stateParams) +
-           "&size=" + $scope.itemsPerPage)
+           "&size=" + $scope.itemsPerPage))
   .then(function(response) {
     $scope.itemsCount = response.data.total;
     $scope.pageCount = PagingService.pageCount($scope.itemsCount, $scope.itemsPerPage);
@@ -21,8 +22,8 @@ function SearchCtrl($scope, $stateParams, $http, $state, PagingService) {
       _(response.data.hits.map(hit => hit._source))
         .chunk(2).toArray();
   })
-  .catch(function(err) {
-    console.log(err); 
+  .catch(err => {
+    $scope.alerts.push({type: 'danger', msg: "Ами сега!? Възникнала е грешка по при комуникацията със сървъра. Моля опитайте отново по-късно. " + err});
   });
 }
 
