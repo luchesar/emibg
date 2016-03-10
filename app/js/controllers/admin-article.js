1f34e89b8ed19e79e4ce9b7be47a43adc570c432
Luchesar Cekov <luchesar@gmail.com>
2016-03-10 17:24:56 +0000

    Fix bug for not being able to paste into the contents of articles and events

--- app/js/controllers/admin-article.js
+++ app/js/controllers/admin-article.js
@@ -91,12 +91,13 @@ function AdminArticleCtrl($scope, $stateParams, ArticleService, $filter, $rootSc
           'google_tools advlist autolink lists link image charmap print preview hr anchor pagebreak',
           'searchreplace wordcount visualblocks visualchars code fullscreen',
           'insertdatetime media nonbreaking save table contextmenu directionality',
-          'template paste textcolor colorpicker textpattern'
+          'template textcolor colorpicker textpattern'
         ],
         toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link google_tools image | print preview media | forecolor',
         image_caption: true,
         image_advtab: true,
         language : "bg",
+        paste_data_images: true
       },
       false
     );
--- app/js/controllers/admin-event.js
+++ app/js/controllers/admin-event.js
@@ -87,7 +87,7 @@ function AdminEventCtrl($scope, $stateParams, EventService, $filter, $rootScope,
           'google_tools advlist autolink lists link image charmap print preview hr anchor pagebreak',
           'searchreplace wordcount visualblocks visualchars code fullscreen',
           'insertdatetime media nonbreaking save table contextmenu directionality',
-          'template paste textcolor colorpicker textpattern'
+          'template textcolor colorpicker textpattern'
         ],
         toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link google_tools image | print preview media | forecolor',
         image_caption: true,
