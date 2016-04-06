4bebe377da8e53c103ea9d888fa657dc69e6cbd0
Luchesar Cekov <luchesar@gmail.com>
2016-04-06 22:38:04 +0100

    Migrate all the articles

--- app/scrap/articles.js
+++ app/scrap/articles.js
@@ -5,26 +5,32 @@ var ObjectId = require("bson-objectid");
 var Rx = require("rx");
 var db = require('mongoskin').db('mongodb://localhost:27017/test');
 
-var newsStream = function() {
-  var newsStream = new Rx.Subject();
+var itemStream = function(indexPage, follow, setter) {
+  var stream = new Rx.Subject();
   osmosis
-  .get('http://www.emi-bg.com/index.php?page=1&class=3')
+  .get(indexPage)
   .paginate('.cat_stranicirane > span:not(.tochki_div):first + a', '.cat_stranicirane > a:last')
-  .follow('.item_block > .cat_title > p.cat_name > a')
-  .set({
-    "title": { "bg": ".news_header > p"},
-    "details": ".div_pod > .cat_autor",
-    "html": {"bg": ".div_pod + div:html"}
-  })
+  .follow(follow)
+  .set(setter)
   .data(function(news) {
-    newsStream.onNext(news);
+    stream.onNext(news);
   })
-  .error(function(err) { newsStream.onError(err); })
-  .log(console.log)
-  .debug(console.log);
+  .error(function(err) { stream.onError(err); });
 
-  return newsStream;
-}
+  return stream;
+};
+
+var articleStream = function(indexPage, follow, titleSelector) {
+  return itemStream(
+    indexPage,
+    follow,
+    {
+      "title": { "bg": titleSelector},
+      "details": ".div_pod > .cat_autor",
+      "html": {"bg": ".div_pod + div:html"}
+    }
+  );
+};
 
 var id = function(millis) {
   var timestamp = Math.floor(millis / 1000);
@@ -32,28 +38,27 @@ var id = function(millis) {
   return new ObjectId(hex + new ObjectId().str.substring(8));
 }
 
-var handleNews = function(news) {
-  var details = news.details;
-  delete news.details;
+var handleArticle = function(article) {
+  var details = article.details;
+  delete article.details;
 
   _(details).split(",").each(function(element) {
     var element = element.trim();
     if (element.indexOf("от:") == 0) {
-      news.author = {};
-      news.author.bg = element.substring(3).trim();
+      article.author = {};
+      article.author.bg = element.substring(3).trim();
     } else if (element.indexOf("дата:") == 0) {
-      news.date = moment(element.substring(5).trim(), "DD.MM.YYYY").valueOf();
-      news.publicationDate = news.date;
+      article.date = moment(element.substring(5).trim(), "DD.MM.YYYY").valueOf();
+      article.publicationDate = article.date;
     }
   });
-  news.id = id(news.date);
-  news._id = news.id;
-  news.itemId = news.id.toString(16);
-  news.category = ["news"];
-  news.migrated = true;
-  news.deleted = false;
-  news.published = true;
-  news.image = {
+  article.id = id(article.date);
+  article._id = article.id;
+  article.itemId = article.id.toString(16);
+  article.migrated = true;
+  article.deleted = false;
+  article.published = true;
+  article.image = {
     "config" : {
       "fill" : true,
       "horizontalAlign" : "center",
@@ -61,8 +66,23 @@ var handleNews = function(news) {
     },
     "url" : "http://placehold.it/120x80"
   }
-  return news;
+  return article;
 };
+
+var handle = function(tag) {
+  return function(news) {
+    var  news = handleArticle(news)
+    news.category = [tag];
+    return news;
+  }
+};
+
+var newsStream = function() {
+  return Rx.Observable.merge(
+    articleStream('http://www.emi-bg.com/index.php?page=1&class=3' ,'.item_block > .cat_title > p.cat_name > a', '.news_header > p'),
+    articleStream('http://www.emi-bg.com/index.php?class=6', ".initiatives_block > .cat_title > p.cat_name > a", '.initiatives_header > p')
+  ).map(handle("news"))
+}
 
 var insert = function(item) {
   if (item.date > moment("10.03.2016", "DD.MM.YYYY").valueOf()) {
@@ -70,15 +90,11 @@ var insert = function(item) {
     return;
   }
   db.collection('articles').insert(item, function(err, result) {
-    if (err) console.log("ERROR cannot add an article with title:" + item.title.bg + JSON.stringify(err));
-    if (result) console.log('SUCCESS adding an article with title:' + item.title.bg);
+    if (err) console.log("ERROR: " + item.title.bg + JSON.stringify(err));
+    if (result) console.log('SUCCESS: ' + item.title.bg);
   });
 };
 
 newsStream()
-.map(handleNews)
 .forEach(insert, function(err) {console.log("err:" + err);});
-//newsStream()
-//.scan(function(acc, next) {return acc + 1;}, 0)
-//.forEach(function(next) { console.log("NEXT:" + next);})
 
