2d56d7a4309c7dc8ff0c50fad6ebcc09ba2003a2
Luchesar Cekov <luchesar@gmail.com>
2016-04-07 07:58:56 +0100

    Adding the emis articles

--- app/scrap/articles.js
+++ app/scrap/articles.js
@@ -1,34 +1,39 @@
-var osmosis = require('osmosis');
 var _ = require("lazy.js");
+var osmosis = require("osmosis");
 var moment = require("moment-timezone");
 var ObjectId = require("bson-objectid");
 var Rx = require("rx");
 var db = require('mongoskin').db('mongodb://localhost:27017/test');
 
-var itemStream = function(indexPage, follow, setter) {
+var itemStream = function(indexPage, follow, setter, paginate) {
   var stream = new Rx.Subject();
-  osmosis
-  .get(indexPage)
-  .paginate('.cat_stranicirane > span:not(.tochki_div):first + a', '.cat_stranicirane > a:last')
+  var osm = osmosis.get(indexPage)
+  if (paginate) {
+    console.log("PAGINATE");
+    osm = osm.paginate('.cat_stranicirane > span:not(.tochki_div):first + a', '.cat_stranicirane > a:last');
+  }
+  osm
   .follow(follow)
   .set(setter)
   .data(function(news) {
     stream.onNext(news);
   })
-  .error(function(err) { stream.onError(err); });
+  .error(function(err) { stream.onError(err); })
 
   return stream;
 };
 
-var articleStream = function(indexPage, follow, titleSelector) {
+var articleStream = function(args) {
+  var paginate = args.paginate === false ? false : true;
   return itemStream(
-    indexPage,
-    follow,
+    args.indexPage,
+    args.follow,
     {
-      "title": { "bg": titleSelector},
+      "title": { "bg": args.titleSelector},
       "details": ".div_pod > .cat_autor",
       "html": {"bg": ".div_pod + div:html"}
-    }
+    },
+    paginate
   );
 };
 
@@ -79,9 +84,38 @@ var handle = function(tag) {
 
 var newsStream = function() {
   return Rx.Observable.merge(
-    articleStream('http://www.emi-bg.com/index.php?page=1&class=3' ,'.item_block > .cat_title > p.cat_name > a', '.news_header > p'),
-    articleStream('http://www.emi-bg.com/index.php?class=6', ".initiatives_block > .cat_title > p.cat_name > a", '.initiatives_header > p')
-  ).map(handle("news"))
+    Rx.Observable.merge(
+      articleStream({
+        indexPage: 'http://www.emi-bg.com/index.php?catid=13' ,
+        follow: '.item_block > .cat_title > p.cat_name > a',
+        titleSelector: '.analysis_header > p'
+      }),
+      articleStream({
+        indexPage: 'http://www.emi-bg.com/index.php?catid=14' ,
+        follow: '.item_block > .cat_title > p.cat_name > a',
+        titleSelector: '.analysis_header > p',
+        paginate: false
+      }),
+      articleStream({
+        indexPage: 'http://www.emi-bg.com/index.php?catid=33',
+        follow: '.item_block > .cat_title > p.cat_name > a',
+        titleSelector: '.analysis_header > p',
+        paginate: false
+      })
+    ).map(handle("emis")),
+    Rx.Observable.merge(
+      articleStream({
+        indexPage: 'http://www.emi-bg.com/index.php?class=3' ,
+        follow: '.item_block > .cat_title > p.cat_name > a',
+        titleSelector: '.news_header > p'
+      }),
+      articleStream({
+        indexPage: 'http://www.emi-bg.com/index.php?class=6',
+        follow: ".initiatives_block > .cat_title > p.cat_name > a",
+        titleSelector: '.initiatives_header > p'
+      })
+    ).map(handle("news"))
+  );
 }
 
 var insert = function(item) {
